27c9d0bc9c0315b10db0fbf3543927425a2b3785
Manoj Kumar <mdnu066@gmail.com>
2025-11-03 06:49:49 +0530

    initial commit

---
+++ src/main/java/com/url/shortener/UrlShortenerSbApplication.java
@@ -0,0 +1,14 @@
+package com.url.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerSbApplication {
+
+	public static void main(String[] args) {
+        // 12
+		SpringApplication.run(UrlShortenerSbApplication.class, args);
+	}
+
+}
